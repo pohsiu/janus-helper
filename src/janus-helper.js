3716f7ac8bbc59752203558507619984350e6765
Louis <louis.hsiao@d8ai.com>
2019-07-25 17:03:27 +0800

    add iceServer comment

--- src/janus-helper.js
+++ src/janus-helper.js
@@ -26,6 +26,15 @@ class JanusHelper {
           this.janus = new Janus(
             {
               server: this.config.server || `wss://${janusHost}:${janusWSSPort}/`,
+              // No "iceServers" is provided, meaning janus.js will use a default STUN server
+              // Here are some examples of how an iceServers field may look like to support TURN
+              // 		iceServers: [{url: "turn:yourturnserver.com:3478", username: "janususer", credential: "januspwd"}],
+              // 		iceServers: [{url: "turn:yourturnserver.com:443?transport=tcp", username: "janususer", credential: "januspwd"}],
+              // 		iceServers: [{url: "turns:yourturnserver.com:443?transport=tcp", username: "janususer", credential: "januspwd"}],
+              // Should the Janus API require authentication, you can specify either the API secret or user token here too
+              //		token: "mytoken",
+              //	or
+              //		apisecret: "serversecret",
               success: () => {
                 console.log('==== create session success ====');
                 resolve(200);
